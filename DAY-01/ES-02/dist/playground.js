8811402b24796a454cbbea1d3b072a0716d50a31
aminelli <antonio.minelli@etlforma.com>
2025-09-08 12:47:12 +0200

    upd

--- DAY-01/ES-02/dist/playground.js
+++ DAY-01/ES-02/dist/playground.js
@@ -4,6 +4,32 @@ const user = {
     name: "Mario",
     roles: ["admin", "user"]
 };
+const product = {
+    id: 101,
+    name: "Laptop",
+    price: 999.99
+};
+class Cart {
+    constructor() {
+        this.items = [];
+    }
+    addItem(item) {
+        this.items.push(item);
+    }
+    removeItem(itemId) {
+        this.items = this.items.filter(item => item.id !== itemId);
+    }
+    getItems() {
+        return this.items;
+    }
+    getTotal() {
+        return this.items.reduce((total, item) => total + item.price, 0);
+    }
+}
+const cart = new Cart();
+cart.addItem(product);
+console.log(cart.getItems());
+console.log("Total:", cart.getTotal());
 console.log("Hello, TypeScript!");
 console.log("Speriamo funzioni tutto correttamente.");
 //# sourceMappingURL=playground.js.map
