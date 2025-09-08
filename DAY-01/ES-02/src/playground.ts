5f76afb5eeff0628c281cb4fdfc6ee3cc1f41803
aminelli <antonio.minelli@etlforma.com>
2025-09-08 11:54:22 +0200

    upd

--- DAY-01/ES-02/src/playground.ts
+++ DAY-01/ES-02/src/playground.ts
@@ -12,5 +12,46 @@ const user: User = {
 }
 
 
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description?: string; // Optional property
+}
+
+const product: Product = {
+    id: 101,
+    name: "Laptop",
+    price: 999.99
+}
+
+
+class Cart {
+    private items: Product[] = [];
+
+    addItem(item: Product): void {
+        this.items.push(item);
+    }
+
+    removeItem(itemId: number): void {
+        this.items = this.items.filter(item => item.id !== itemId);
+    }
+
+    getItems(): Product[] {
+        return this.items;
+    }
+
+    getTotal(): number {
+        return this.items.reduce((total, item) => total + item.price, 0);
+    }
+
+}
+
+const cart = new Cart();
+cart.addItem(product);
+console.log(cart.getItems());
+console.log("Total:", cart.getTotal()); 
+
+
 console.log("Hello, TypeScript!");
 console.log("Speriamo funzioni tutto correttamente.");
