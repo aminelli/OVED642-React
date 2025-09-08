d3017ed1073593818202c09db26a9417a95ccf4f
aminelli <antonio.minelli@etlforma.com>
2025-09-08 10:35:00 +0200

    upd

---
+++ DAY-01/ES-01/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/data', (req, res) => {
+  const data = req.body;
+  res.json({ received: data });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
